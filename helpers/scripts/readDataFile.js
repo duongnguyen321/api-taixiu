327839c90efee0661b98da4d5983d0128e068037
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-06-01 01:49:32 +0700

    update logic read, write

--- helpers/scripts/readDataFile.js
+++ helpers/scripts/readDataFile.js
@@ -1,13 +1,13 @@
-// Trong file helper/readDataFile.js
 const fs = require("fs").promises;
 
 async function readDataFile(filePath) {
   try {
-    const jsonString = await fs.readFile(filePath, "utf8");
-    return JSON.parse(jsonString);
+    const fileContent = await fs.readFile(filePath, "utf8");
+    const dataObject = JSON.parse(fileContent);
+    return dataObject;
   } catch (err) {
-    console.log("Error reading file:", err);
-    throw err;
+    console.log("Error reading data file:", err);
+    throw new Error("Failed to read data file");
   }
 }
 
--- helpers/scripts/writeDataFile.js
+++ helpers/scripts/writeDataFile.js
@@ -1,12 +1,13 @@
-// Trong file helper/writeDataFile.js
 const fs = require("fs").promises;
 
 async function writeDataFile(filePath, dataObject) {
   try {
-    await fs.writeFile(filePath, JSON.stringify(dataObject));
-    console.log("Data has been updated.");
+    const jsonString = JSON.stringify(dataObject);
+    await fs.writeFile(filePath, jsonString);
+    console.log("Data file has been updated");
   } catch (err) {
-    console.log("Error writing file:", err);
+    console.log("Error writing data file:", err);
+    throw new Error("Failed to write data file");
   }
 }
 
